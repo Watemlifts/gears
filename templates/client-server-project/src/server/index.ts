d9362e06088240e58f24b6a71218071b56304ab7
sinclairzx81 <haydn.developer@gmail.com>
2019-02-05 04:35:36 +1300

    update to smoke-task

--- templates/client-server-project/src/server/index.ts
+++ templates/client-server-project/src/server/index.ts
@@ -1,3 +1,5 @@
 // note: the client project is listening on port 5000
 // so the server should listen and expose its API's on 
 // other ports.
+
+console.log('hello world')
--- templates/client-server-project/tasks.js
+++ templates/client-server-project/tasks.js
@@ -1,39 +1,10 @@
-//------------------------------------------------------
-// task helper scripts:
-//------------------------------------------------------
-const shell = command =>
-  new Promise((resolve, reject) => {
-    const { spawn } = require('child_process')
-    const windows = /^win/.test(process.platform)
-    console.log(`\x1b[32m${command}\x1b[0m`)
-    const ls = spawn(windows ? 'cmd' : 'sh', [windows ? '/c' : '-c', command])
-    ls.stdout.pipe(process.stdout)
-    ls.stderr.pipe(process.stderr)
-    ls.on('close', code => resolve(code))
-  })
-
-const cli = async (args, tasks) => {
-  const task = args.length === 3 ? args[2] : 'none'
-  const func = tasks[task]
-    ? tasks[task]
-    : () => {
-        console.log('tasks:')
-        Object.keys(tasks).forEach(task => console.log(` - ${task}`))
-      }
-  await func()
-}
-
-//------------------------------------------------------
-// tasks:
-//------------------------------------------------------
-
-async function clean() {
+export async function clean() {
   await shell('shx rm -rf ./bin/index.js')
   await shell('shx rm -rf ./bin/public/index.js')
   await shell('shx rm -rf ./node_modules')
 }
 
-async function start() {
+export async function start() {
   // pre-build project
   await shell('npm install')
   await shell('tsc-bundle ./src/client/tsconfig.json')
@@ -47,12 +18,3 @@ async function start() {
     shell('fsweb ./bin/public/')
   ])
 }
-
-//------------------------------------------------------
-//  cli:
-//------------------------------------------------------
-
-cli(process.argv, {
-  clean,
-  start
-}).catch(console.log)
